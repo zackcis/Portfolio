683eae46a8e40559b7fd74c1025cff418202e3c6
zackcis <zakaria.eldahar@gmail.com>
2024-04-29 23:49:45 +0100

    responsivity

--- src/pages/Home/components/firstSection.jsx
+++ src/pages/Home/components/firstSection.jsx
@@ -81,35 +81,38 @@ export const FirstSection = () => {
     return (
         <>
 
-            <nav class="bg-[#101630] w-[100%]  flex justify-center items-center pt-[5vh]">
+            <nav class="bg-[#101630] w-[100%]  flex justify-center items-center pt-[5vh] ">
                 <div class="max-w-screen-xl  flex flex-wrap items-center justify-between mx-auto ">
                     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                         <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li className='cursor-pointer'>
-                                <Link smooth={true} duration={500}  to="Home" class="block py-2 px-8 text-white hover:text-[#1ed760] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ">Home</Link>
+                                <Link smooth={true} duration={500} to="Home" class="block py-2 px-8 text-white hover:text-[#1ed760] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ">Home</Link>
                             </li>
                             <li className='cursor-pointer'>
-                                <Link to="about" smooth={true} duration={500}  class="block py-2 px-8 text-white hover:text-[#1ed760] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ">About</Link>
+                                <Link to="about" smooth={true} duration={500} class="block py-2 px-8 text-white hover:text-[#1ed760] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ">About</Link>
                             </li>
                             <li className='cursor-pointer'>
-                                <Link smooth={true} duration={500}  to="project" class="block py-2 px-8 text-white hover:text-[#1ed760] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ">Projects</Link>
+                                <Link smooth={true} duration={500} to="project" class="block py-2 px-8 text-white hover:text-[#1ed760] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ">Projects</Link>
                             </li>
                             <li className='cursor-pointer'>
                                 <Link smooth={true} duration={500} to="skills" class="block py-2 px-8 text-white hover:text-[#1ed760] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ">Skills</Link>
                             </li>
                             <li className='cursor-pointer'>
-                                <Link smooth={true} duration={500}  to="contact" class="block py-2 px-8 text-white hover:text-[#1ed760] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ">Contact</Link>
+                                <Link smooth={true} duration={500} to="contact" class="block py-2 px-8 text-white hover:text-[#1ed760] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ">Contact</Link>
                             </li>
                         </ul>
                     </div>
                 </div>
             </nav>
-            <div  name="Home" data-aos="fade-right" data-aos-duration="2500" className='bg-[#101630] w-[100%] h-screen flex justify-center items-center '>
-                <div className='w-[100%] h-[70%] flex flex-col justify-around items-center'>
-                    <p className=' w-[50%] text-start text-5xl text-gray-500 '>
-                        <span className='font-bold leading-[100px]  '>I'm a</span><br /> <span className='text-white kbira  text-7xl'>Full-Stack Software Developer .</span>
+
+
+
+            <div name="Home" data-aos="fade-right" data-aos-duration="2500" className='bg-[#101630] w-[100%] h-screen flex justify-center items-center max-[430px]:max-w-[100%] max-[430px]:flex-col-reverse max-[430px]:justify-center max-[430px]:items-center'>
+                <div className='w-[100%] h-[60%] flex flex-col justify-around items-center max-[430px]:flex-col max-[430px]:justify-center max-[430px]:items-center max-[430px]:text-center'>
+                    <p className=' w-[50%] text-start text-5xl text-gray-500 max-[430px]:mb-5  max-[430px]:text-center'>
+                        <span className='font-bold leading-[100px]  '>I'm a</span><br /> <span className='text-white kbira  text-7xl max-[430px]:text-3xl '>Full-Stack Software Developer .</span>
                     </p>
-                    <div className='w-[50%] flex justify-start'>
+                    <div className='w-[50%] flex justify-start max-[430px]:justify-center'>
                         <button className='items-start p-[30px] rounded-full bg-gradient-to-r from-[#49b0d8] from-10% via-sky-500 via-30% to-[#3a2c8f] to-90% text-white font-bold '>Previous Project</button>
                     </div>
                 </div>
@@ -117,13 +120,16 @@ export const FirstSection = () => {
                     <img src={ana} alt="" className='w-[350px] imagi ' />
                 </div>
             </div>
-            <div name="about" className='about w-[100%]  bg-[#101630] h-[70vh]  flex justify-center items-center' data-aos="fade-left" data-aos-duration="2500">
-                <div className='w-[90%] h-[100%] flex justify-center items-center'>
-                    <div className='w-[80%] h-[100%] flex justify-between items-center text-center gap-8'>
-                        <div className='about-image w-[70%] '>
+
+
+
+            <div name="about" className='about w-[100%]  bg-[#101630] h-[70vh] max-[430px]:h-[100vh] max-[430px]:mb-10  max-[430px]:py-3  flex justify-center items-center' data-aos="fade-left" data-aos-duration="2500">
+                <div className='w-[90%] max-[430px]:w-[100%] h-[100%] flex justify-center items-center '>
+                    <div className='w-[80%] max-[430px]:w-[100%] h-[100%] flex justify-between items-center text-center gap-8 max-[430px]:flex-col'>
+                        <div className='about-image w-[70%] max-[430px]:w-[40%] '>
                             <img src={abouti} className='w-[100%] about-image' alt="" />
                         </div>
-                        <div className='about-script flex flex-col justify-around items-center text-center h-[60%] '>
+                        <div className='about-script flex flex-col justify-around items-center text-center h-[60%] max-[430px]:w-[100%] '>
                             <h1 className='kbira text-white text-3xl'>About</h1>
                             <p className='w-[70%] text-2xl text-white '>
                                 Front End Web Developer passionate about software development and crafting engaging user experiences. Demonstrated expertise in client interaction and project management honed through freelancing endeavors. Committed to advancing front-end skills and staying abreast of emerging technologies to deliver exceptional digital solutions.
@@ -132,16 +138,18 @@ export const FirstSection = () => {
                     </div>
                 </div>
             </div>
-            <div name="project" className='projects w-[100%]  bg-[#101630] h-[70vh]  
-            flex flex-col justify-around items-center' data-aos="fade-down" data-aos-duration="2500">
+
+
+
+            <div name="project" className='projects w-[100%]  bg-[#101630] h-[70vh] max-[430px]:h-[230vh]  flex flex-col justify-around items-center  ' data-aos="fade-down" data-aos-duration="2500">
                 <h1 className='font-bold text-center text-white text-3xl kbira'>
                     PROJECTS
                 </h1>
-                <div className='w-[100%]  flex flex-wrap justify-center items-center'>
+                <div className='w-[100%] flex flex-wrap justify-center items-center max-[430px]:flex-col max-[430px]:h-[98%]  max-[430px]:gap-[80px]'>
                     {
                         arrayi.map(element =>
 
-                            <div className='cards relative  w-[25%] h-[100%] flex flex-wrap justify-around'>
+                            <div className='cards relative  w-[25%] h-[100%] max-[430px]:h-[27%] flex flex-wrap justify-around  max-[430px]:w-[100%] '>
                                 <div class="max-w-sm shadow-lg shadow-white bg-[#101630] rounded-lg  dark:bg-gray-800 dark:border-gray-700">
                                     <img class="rounded-t-lg" src={element.picture} alt="" className='  ' />
                                     <div class="p-5 flex flex-col justify-between ">
@@ -164,14 +172,17 @@ export const FirstSection = () => {
                         )
                     }
                 </div>
-
-
             </div>
-            <div name="skills" data-aos="fade-left" data-aos-duration="2500" className='skills  w-[100%]  bg-[#101630] h-[50vh]   flex flex-col justify-center items-center'>
+
+
+
+
+
+            <div name="skills" data-aos="fade-left" data-aos-duration="2500" className='skills   w-[100%]  bg-[#101630] h-[50vh] max-[430px]:flex-col  max-[430px]:h-[125vh] max-[430px]:p-0     flex flex-col justify-center items-center'>
                 <h1 className='font-bold text-center text-white kbira text-3xl'>Skills</h1>
-                <div className='icons flex flex-wrap justify-around items-center gap-10 h-[80%]'>
+                <div className='icons flex flex-wrap justify-around items-center gap-10 h-[80%] '>
                     {skill.map(skyll =>
-                        <div className='flex flex-col justify-center items-center'>
+                        <div className='flex flex-col justify-center items-center max-[430px]:flex-col'>
                             <img src={skyll.picture} alt="" className='w-[170px] p-10' />
                             <h2 className='kbira 2xl text-white'>{skyll.title}</h2>
                         </div>
@@ -180,18 +191,24 @@ export const FirstSection = () => {
 
                 </div>
             </div>
+
+
+
+
+
             <div name="contact" className='bg-[#101630] '>
-                <div className='rounded-t-[200px] contact w-[100%] bg-gradient-to-r from-[#4733bccb] from-10% via-sky-500 via-30% to-[#4f97d6bb] to-90% h-[70vh]  flex flex-col justify-center items-center '>
-                    <h1 className='text-2xl text-white kbira'>
+                <div className='rounded-t-[200px] contact w-[100%] bg-gradient-to-r from-[#4733bccb] from-10% via-sky-500 via-30% to-[#4f97d6bb] to-90% h-[70vh]  flex flex-col justify-center items-center max-[430px]:h-[180vh]
+                '>
+                    <h1 className='text-2xl text-white kbira max-[430px]:text-center'>
                         Contact
                     </h1>
-                    <div className='w-[50%] h-[80%] flex justify-start items-center'>
-                        <div className='drop-message flex flex-col gap-7 justify-start items-start'>
-                            <h1 className='kbira text-2xl text-white'>Drop a message</h1>
-                            <p className='w-[60%]'>
+                    <div className='w-[100%] h-[80%] flex justify-start items-center max-[430px]:flex-col  max-[430px]:items-center max-[430px]:gap-[100px]  max-[430px]:justify-center'>
+                        <div className='drop-message flex flex-col gap-7 justify-start items-start max-[430px]:justify-center max-[430px]:items-center'>
+                            <h1 className='kbira text-2xl text-white max-[430px]:text-center'>Drop a message</h1>
+                            <p className='w-[60%] max-[430px]:text-center'>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, distinctio. Ex beatae quaerat voluptates impedit facere. Velit veritatis dolore accusamus!
                             </p>
-                            <div className='tele flex justify-start items-center w-[60%] gap-6'>
+                            <div className='tele flex justify-start items-center w-[60%] gap-6 max-[430px]:flex-col max-[430px]:justify-center max-[430px]:items-center'>
                                 <div className='bg-gradient-to-r from-[#4733bccb] from-10%  via-30% to-[#0ea4e8] to-90% text-white w-[50px] h-[50px] rounded-lg flex justify-center items-center'>
                                     <FaSquarePhone />
                                 </div>
@@ -199,7 +216,7 @@ export const FirstSection = () => {
                                     +212699312362
                                 </p>
                             </div>
-                            <div className='message flex justify-start items-center w-[60%] gap-6'>
+                            <div className='message flex justify-start items-center w-[60%] gap-6 max-[430px]:flex-col max-[430px]:justify-center max-[430px]:items-center'>
                                 <div className='bg-gradient-to-r from-[#4733bccb] from-10%  via-30% to-[#0ea4e8] to-90% text-white w-[50px] h-[50px] rounded-lg flex justify-center items-center'>
                                     <BiSolidMessageDetail />
                                 </div>
@@ -207,16 +224,16 @@ export const FirstSection = () => {
                                     zakaria.eldahar@gmail.com
                                 </p>
                             </div>
-                            <div className='local flex justify-start items-center w-[60%] gap-6 '>
+                            <div className='local flex justify-start items-center w-[60%] gap-6 max-[430px]:flex-col max-[430px]:justify-center max-[430px]:items-center '>
                                 <div className='bg-gradient-to-r from-[#4733bccb] from-10%  via-30% to-[#0ea4e8] to-90% text-white w-[80px] h-[50px] rounded-lg flex justify-center items-center'>
                                     <FaLocationDot />
                                 </div>
-                                <p>
+                                <p className='max-[430px]:text-center'>
                                     Morocco,Casablanca,Sidi moumen Boulvard houssein soussi, Rue 29
                                 </p>
                             </div>
                         </div>
-                        <div class="max-w-md p-8 bg-gray-800 rounded-md shadow-md form-container w-[70%]">
+                        <div class="max-w-md p-8 bg-gray-800 rounded-md shadow-md form-container w-[70%] max-[430px]:w-[90%]">
                             <h2 class="text-2xl font-semibold text-white mb-6">Say Something!</h2>
                             <form className='overflow-x-hidden' action="https://fabform.io/f/insert-form-id" method="POST">
                                 <div class="mb-4">
